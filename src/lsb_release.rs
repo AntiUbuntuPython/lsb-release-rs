80dcb99e185f1255a11498a7b0c237aa1c355ec2
KisaragiEffective <kisaragi.effective@gmail.com>
2022-05-29 12:51:55 +0900

    feat: fully implemented, maybe

--- src/lsb_release.rs
+++ src/lsb_release.rs
@@ -29,19 +29,19 @@ static modnamare: Lazy<Regex> = Lazy::new(|| Regex::new(r#"lsb-(?P<module>[a-z\d
 // replacement for /usr/share/pyshared/lsb_release.py
 impl LSBInfo for LSBInfoGetter {
     fn id(&self) -> Option<String> {
-        todo!()
+        get_distro_information().ok().and_then(|a| a.id)
     }
 
     fn description(&self) -> Option<String> {
-        todo!()
+        get_distro_information().ok().and_then(|a| a.description)
     }
 
     fn release(&self) -> Option<String> {
-        todo!()
+        get_distro_information().ok().and_then(|a| a.release)
     }
 
     fn codename(&self) -> Option<String> {
-        todo!()
+        get_distro_information().ok().and_then(|a| a.codename)
     }
 
     // this is check_modules_installed()
