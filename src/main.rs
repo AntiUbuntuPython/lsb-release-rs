2447328c2c5de2e9fc2a9d2bb262c826a0338566
KisaragiEffective <kisaragi.effective@gmail.com>
2022-05-29 13:16:23 +0900

    fix: clippy warning

--- src/main.rs
+++ src/main.rs
@@ -18,56 +18,56 @@ fn main() {
     if args.show_lsb_modules {
         match grub.lsb_version() {
             None => {
-                eprintln!("No LSB modules are available.")
+                eprintln!("No LSB modules are available.");
             }
             Some(lsb_version) => {
                 let v = lsb_version.join(":");
                 if short {
-                    println!("{v}")
+                    println!("{v}");
                 } else {
-                    println!("LSB Version: \t{v}")
+                    println!("LSB Version: \t{v}");
                 }
             }
         }
     }
 
     if args.show_distributor {
-        let v = grub.id().unwrap_or(na.clone());
+        let v = grub.id().unwrap_or_else(|| na.clone());
 
         if short {
-            println!("{v}")
+            println!("{v}");
         } else {
-            println!("Distributor ID:\t{v}")
+            println!("Distributor ID:\t{v}");
         }
     }
 
     if args.show_description {
-        let v = &grub.description().unwrap_or(na.clone());
+        let v = &grub.description().unwrap_or_else(|| na.clone());
 
         if short {
-            println!("{v}")
+            println!("{v}");
         } else {
-            println!("Description:\t{v}")
+            println!("Description:\t{v}");
         }
     }
 
     if args.show_release {
-        let v = &grub.release().unwrap_or(na.clone());
+        let v = &grub.release().unwrap_or_else(|| na.clone());
 
         if short {
-            println!("{v}")
+            println!("{v}");
         } else {
-            println!("Release:\t{v}")
+            println!("Release:\t{v}");
         }
     }
 
     if args.show_codename {
-        let v = &grub.codename().unwrap_or(na.clone());
+        let v = &grub.codename().unwrap_or_else(|| na.clone());
 
         if short {
-            println!("{v}")
+            println!("{v}");
         } else {
-            println!("Codename:\t{v}")
+            println!("Codename:\t{v}");
         }
     }
 }
